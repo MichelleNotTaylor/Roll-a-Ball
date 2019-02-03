0dfdd56f6791ed370191b263afa67a72e99e2f46
Michelle Taylor <mmtaylor3@dmacc.edu>
2019-02-03 17:22:51 -0600

    Complete Roll a Ball game.

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
 	public float speed;
 	public Text countText;
+	public Text winText;
+
 	private Rigidbody rb;
 	private int count;
 
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour
 		rb = GetComponent<Rigidbody>();
 		count = 0;
 		SetCountText();
+		winText.text = "";
     }
 
     void FixedUpdate()
@@ -22,12 +24,12 @@ public class PlayerController : MonoBehaviour
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 
-		Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce(movement * speed);
     }
 
-	private void OnTriggerEnter(Collider other)
+	void OnTriggerEnter(Collider other)
 	{
 		if(other.gameObject.CompareTag("Pick Up"))
 		{
@@ -40,5 +42,9 @@ public class PlayerController : MonoBehaviour
 	void SetCountText()
 	{
 		countText.text = "Count: " + count.ToString();
+		if (count >= 8)
+		{
+			winText.text = "You Win!";
+		}
 	}
 }
